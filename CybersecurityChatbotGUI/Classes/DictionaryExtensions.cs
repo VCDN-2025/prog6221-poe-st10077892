ebd0fc4ac543c0e4c2924aabe675f22006917536
st10077892 <128598034+st10077892@users.noreply.github.com>
2025-06-26 21:11:22 +0200

    added in all my code attributes

--- CybersecurityChatbotGUI/Classes/DictionaryExtensions.cs
+++ CybersecurityChatbotGUI/Classes/DictionaryExtensions.cs
@@ -2,6 +2,41 @@
 using System.Collections.Generic;
 using System.Linq;
 
+//Name: WPF Coffee Shop App
+//Author: fb-shaik
+//Link: https://github.com/fb-shaik/PROG6221-Group2-2025/tree/main/WPF_Coffee_Shop_App
+//Date accessed: 24 June 2025
+
+//Name: WPF Demo App
+//Author: fb-shaik
+//Link: https://github.com/fb-shaik/PROG6221-Group2-2025/tree/main/WPF_Demo_App
+//Date accessed: 24 June 2025
+
+//Name: Create a bot with the Bot Framework SDK
+//Author: Microsoft
+//Link: https://learn.microsoft.com/en-us/azure/bot-service/bot-service-quickstart-create-bot?view=azure-bot-service-4.0&tabs=csharp%2Cvs
+//Date accessed: 24 June 2025
+
+//Name: C# Inheritance
+//Author: W3Schools
+//Link:https://www.w3schools.com/cs/cs_inheritance.php
+//Date accessed: 24 June 2025
+
+//Name: Types of Cyber Attacks
+//Author: Check Point
+//Link: https://www.checkpoint.com/cyber-hub/cyber-security/what-is-cyber-attack/types-of-cyber-attacks/
+//Date accessed: 24 June 2025
+
+//Name: SoundPlayer Class
+//Author: Microsoft
+//Link: https://learn.microsoft.com/en-us/dotnet/api/system.media.soundplayer?view=windowsdesktop-9.0
+//Date accessed: 24 June 2025
+
+//Name: Validating Input
+//Author: Makers Institute
+//Link: https://makersinstitute.gitbooks.io/c-basics/content/validating-input.html
+//Date accessed: 24 June 2025
+
 namespace CybersecurityChatbotGUI.Classes
 {
     static class DictionaryExtensions
